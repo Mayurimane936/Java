6efd5b3f52cf960cdaf74604cc9b23352777fcca
Mayuri <mayurimane936@gmail.com>
2025-04-12 19:34:13 +0530

    Used string data structure to check how it works

---
+++ StringJava.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class StringJava {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String m;
+        System.out.println("May I know your name : ");
+        m = s.next();
+        System.out.println("helloo "+ m);
+    }
+}
